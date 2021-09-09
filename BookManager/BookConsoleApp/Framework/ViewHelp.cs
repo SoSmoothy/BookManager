6ee6334a38c5b5c22da61439691a5b8d87eca6d6
Pham Anh Duc <88713288+SoSmoothy@users.noreply.github.com>
2021-09-09 20:39:33 +0700

    update

---
+++ BookManager/BookConsoleApp/Framework/ViewHelp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BookConsoleApp.Framework
+{
+    public class ViewHelp
+    {
+        public static void WriteLine(string message, ConsoleColor color = ConsoleColor.White, bool resetColor = true)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            if (resetColor)
+            {
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ BookManager/BookConsoleApp/Views/BookUpdateView.cs
@@ -0,0 +1,18 @@
+﻿namespace BookConsoleApp.Views
+{
+    using Models;
+    public class BookUpdateView
+    {
+        protected Book Model;
+
+        public BookUpdateView(Book model)
+        {
+            Model = model;
+        }
+
+        public void Render()
+        {
+            
+        }
+    }
+}
